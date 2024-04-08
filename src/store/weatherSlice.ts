3f1adc0e57f138789b9a2dcff5b9ae04f29d2ac1
Frominor <miha.mosp@yandex.ru>
2024-04-08 20:14:10 +0300

    f

--- src/store/weatherSlice.ts
+++ src/store/weatherSlice.ts
@@ -24,7 +24,7 @@ export const FindCityCoords = createAsyncThunk(
   "weather/fetchByCityName",
   async (CityName: String) => {
     const response = await axios.get(
-      `https://api.openweathermap.org/geo/1.0/direct?q=${CityName}&limit=5&appid=${process.env.REACT_APP_API_KEY}`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${CityName}&limit=5&appid=${process.env.REACT_APP_API_KEY}`
     );
     return response.data;
   }
